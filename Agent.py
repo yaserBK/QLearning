f358f6775119bc24bfbf422f7701763122f7f0c5
Yaser <myaserbk@gmail.com>
2020-11-11 02:29:03 +0000

    Slight update

--- Agent.py
+++ Agent.py
@@ -9,19 +9,20 @@ import random
 
 class Agent:
     debug = True
+    alpha = float(0.1)
+    gamma = 0.5
+    epsilon = 0.1
+    # 0 == defect, 1 == cooperate
+    action_labels = ["D", "C"]
+    num_actions = 2
 
     # init method
-    def __init__(self, name, alpha, gamma, epsilon, num_actions, action_labels):
-        self.name = name  # name of the agent
-        self.alpha = alpha  # learning rate
-        self.gamma = gamma  # discount factor
-        self.epsilon = epsilon  # probability of random action/exploration
-        self.num_actions = num_actions  # the number of available actions in the environment
-        self.action_labels = action_labels  # labels/names of all available actions
-        self.q_table = np.zeros((1, num_actions))  # can be modified later for problems with multiple states.
+    def __init__(self, name):
+        self.name = name
+        self.q_table = np.zeros((1, self.num_actions))  # can be modified later for problems with multiple states.
 
     # method to return 1 for cooperate and 2 for defect
-    def select_action(self):
+    def ol_select_action(self):
         # selected_action = None  # declaring variable to be returned later
         random_value = random.random()
         if self.debug:
@@ -36,25 +37,36 @@ class Agent:
             print("Agent", self.name, ": selected action", self.action_labels[selected_action], "greedily")
         return selected_action
 
+    def select_action(self):
+        random_value = random.random()
+        if self.debug:
+            print("Agent", self.name, ": selecting action, epsilon =", self.epsilon, "random Value =", random_value)
+
+        if random_value < self.epsilon:
+            selected_action = self.select_random_action()
+            print("Agent", self.name, ": selected action", self.action_labels[selected_action], "at random")
+        else:
+            selected_action = self.get_max_valued_action()
+            print("Agent", self.name, ": selected action", self.action_labels[selected_action], "greedily")
+        return selected_action
+
     # function to return a random action
     # called in the select_action() function
     def select_random_action(self):
         return int(random.random() * self.num_actions)
 
-    # function to return a max valued action
-    # called in the select_action() function
     def get_max_valued_action(self):
-        # since the q_table here only has a single dimension the row (as there is only state 0)
-        # is returned
-        max_index_column = np.argmax(self.q_table, 0)
-        # the following code can be uncommented for use in problems with states
-        # max_index_row = np.argmax(self.q_table, 1) # (___, 1) returns row position of element
-        # return (max_index_row, max_index_column)
-        return max_index_column
+        if self.q_table[0, 0] > self.q_table[0, 1]:
+            return 0
+        elif self.q_table[0, 0] < self.q_table[0, 1]:
+            return 1
+        else:
+            return int(random.random()*self.num_actions)
 
     def enable_debugging(self):
         self.debug = True
 
+    # function used in update_q_value()
     def get_max_q_value(self):
         index = self.get_max_valued_action()
         return self.q_table[0, index]
@@ -62,5 +74,5 @@ class Agent:
     def update_q_value(self, selected_action, reward):
         old_q = self.q_table[0, selected_action]
         max_q = self.get_max_q_value()
-        new_q = float(old_q + self.alpha * (reward + self.gamma * max_q - old_q))
+        new_q = old_q + self.alpha * (reward + self.gamma * max_q - old_q)
         self.q_table[0, selected_action] = new_q
--- Game.py
+++ Game.py
@@ -20,8 +20,9 @@ class Game:
     # init for prisoners dilemma specifically
     def __init__(self):
         # constructor takes in two agents that are meant to be instantiated in the "main"
-        self.agent_a = Agent("A", 0.1, 0.9, 0.1, 2, self.pd_a_labels)
-        self.agent_b = Agent("B", 0.1, 0.9, 0.1, 2, self.pd_a_labels)
+        # agent constructor takes: agent name, alpha, gamma, epsilon, num_actions, action_labels.
+        self.agent_a = Agent("A")
+        self.agent_b = Agent("B")
 
     # after a round is played each players payoff will be used to update their respective q_tables.
     # this function can be copied and modified for other games
@@ -29,25 +30,33 @@ class Game:
         # each agent chooses an action at the start of round
         payoff = None
         action_a = self.agent_a.select_action()
-        print("ACTION SELECTED IN GAME: ", action_a)
+        print("ACTION SELECTED IN GAME BY A: ", action_a)
         action_b = self.agent_b.select_action()
+        print("ACTION SELECTED IN GAME BY B: ", action_b)
 
         # both cooperate
+        payoff_a = 0
+        payoff_b = 0
         if action_a == 1 and action_b == 1:
-            payoff = self.pd_payoffs[0, 0]
+            payoff_a = 3
+            payoff_b = 3
         elif action_a == 0 and action_b == 0:
-            payoff = self.pd_payoffs[0, 3]
+            payoff_a = 0
+            payoff_b = 0
         # a cooperates, b defects:
         elif action_a == 1 and action_b == 0:
-            payoff = self.pd_payoffs[0, 1]
+            payoff_a = 0
+            payoff_b = 3
         # a defects, b cooperates:
         elif action_a == 0 and action_b == 1:
-            payoff = self.pd_payoffs[0, 2]
+            payoff_a = 3
+            payoff_b = 0
 
-        payoff_a, payoff_b = payoff
+        # payoff_a, payoff_b = payoff
         self.agent_a.update_q_value(action_a, payoff_a)
+        print("Payoff A: ", payoff_a)
+        print(self.agent_a.q_table)
         self.agent_b.update_q_value(action_b, payoff_b)
-
-
-
+        print("Payoff B: ", payoff_b)
+        print(self.agent_b.q_table)
 
